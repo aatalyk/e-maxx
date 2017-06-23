08c808373cc77abe5f5f9b3c62056bc048613669
new_user_name <new@email.com>
2017-06-23 14:59:58 +0600

    Initial Commit

---
+++ e-maxx.ru/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  e-maxx.ru
+//
+//  Created by Admin on 6/23/17.
+//  Copyright © 2017 AAkash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ e-maxx.ru/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  e-maxx.ru
+//
+//  Created by Admin on 6/23/17.
+//  Copyright © 2017 AAkash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
